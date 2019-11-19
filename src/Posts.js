246b059415dacf11b56885131ae74b3c9ddb6382
Stav Barak <stavb@tikalk.com>
2019-11-19 21:32:25 +0200

    edit button

--- src/Posts.js
+++ src/Posts.js
@@ -3,13 +3,12 @@ import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-adm
 
 export const PostList = props => (
     <List {...props}>
-        <Datagrid rowClick="edit">
+        <Datagrid>
             <TextField source="id" />
             <ReferenceField source="userId" reference="users">
                 <TextField source="name" />
             </ReferenceField>
             <TextField source="title" />
-            <TextField source="body" />
             <EditButton />
         </Datagrid>
     </List>
