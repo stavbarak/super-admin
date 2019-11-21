5a2026bfefe9a12b7a2f20af6332bd46f3d630b5
Stav Barak <stavb@tikalk.com>
2019-11-21 21:15:40 +0200

    edit and auth provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { UserList } from './Users';
-import { PostList } from './Posts';
+import { UserList } from './components/Users';
+import { PostList, PostEdit, PostCreate } from './components/Posts';
+import Dashboard from './components/Dashboard';
+import authProvider from './authProvider';
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
-    <Resource name="users" list={UserList} />
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
