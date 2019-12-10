95ce2e72b41b0f7e0d900fcc80f5a6aa022070d7
Stav Barak <stavb@tikalk.com>
2019-12-10 18:28:08 +0200

    data providers folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import restProvider from './data-providers/restProvider';
 import { UserList } from './components/Users';
 import { PostList, PostEdit, PostCreate } from './components/Posts';
 import Dashboard from './components/Dashboard';
@@ -9,10 +10,10 @@ import authProvider from './authProvider';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+//const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={restProvider} >
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
