1f14cab3c7cf324de3b49fa28e13c9e4f6e30836
Stav Barak <stavb@tikalk.com>
2019-12-19 20:23:10 +0200

    trying things

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import authProvider from './authProvider';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-//const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={restProvider} >
+  <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
